843e2b748c1ba5b9c1dc43a26e307f6cccdc640f
Banyapon Poolsawas <daydevthailand@gmail.com>
2026-02-27 10:26:22 +0700

    Refactor addCoupon.js for new coupon minting
    Updated coupon minting process to use new contract address and added a new coupon with specific details.

--- chapter5/scripts/addCoupon.js
+++ chapter5/scripts/addCoupon.js
@@ -1,28 +1,23 @@
 const hre = require("hardhat");
-
 async function main() {
-  const [owner] = await hre.ethers.getSigners();
-  const contractAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-  const Coupon = await hre.ethers.getContractFactory("SimpleCoupon");
-  const coupon = await Coupon.attach(contractAddress);
-
-  console.log(`Minting coupons from owner account: ${owner.address}...`);
-  await coupon.connect(owner).awardCoupon(
-    owner.address,
-    "COUPON099",
-    "ส่วนลด 20% สำหรับการสั่งซื้อครั้งที่สอง,สินค้าใดก็ได้ของห้าง",
-    "https://peach-common-hare-754.mypinata.cloud/ipfs/bafybeibzs35ce2smfphrsxjzmi23kpjivq4earcdk467mgxybiopugo45a"
-  );
-  console.log("Minted coupon with code: COUPON099");
-  await coupon.connect(owner).awardCoupon(
-    owner.address,"COUPON099","ส่วนลด 20% สำหรับการสั่งซื้อครั้งที่สอง,สินค้าใดก็ได้ของห้าง",
-    "https://peach-common-hare-754.mypinata.cloud/ipfs/bafybeibzs35ce2smfphrsxjzmi23kpjivq4earcdk467mgxybiopugo45a"
-  );
-  console.log("Minted coupon with code: SUMMER25");
-  console.log("Coupons minted and assigned successfully!");
+    const [deployer] = await hre.ethers.getSigners();
+    const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+    const Coupon = await hre.ethers.getContractFactory("SimpleCoupon");
+    const coupon = await Coupon.attach(contractAddress);
+    //แก้ไขข้อมูล coupon ที่ต้องการ mint
+    const recipient = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    // ใส่ address ผู้รับคูปอง
+    const code = "COUPON2";
+    const description = "ลด 20% เมื่อซื้อครบ 1,200 บาท";
+    const image = "https://cyan-defeated-booby-344.mypinata.cloud/ipfs/bafybeibvd4dgytisz2fuug7q7axvbd47hhbz2jxgoultpepbo7miql2rxa/coupon1.jpg"
+    const tx = await coupon.awardCoupon(recipient, code, description, image);
+    await tx.wait();
+    console.log(`Coupon "${code}" awarded to ${recipient} \nDescription: ${description} \nImage: ${image}`);
+    console.log("Transaction hash:", tx.hash);
 }
 
+
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
