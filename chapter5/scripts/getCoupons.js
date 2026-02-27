09d93933de2fbb83377922a7eaa8f1fa2baa6312
Banyapon Poolsawas <daydevthailand@gmail.com>
2026-02-27 10:26:44 +0700

    Refactor getCoupons.js to improve coupon display
    Updated the contract address and modified the output format for displaying coupons.

--- chapter5/scripts/getCoupons.js
+++ chapter5/scripts/getCoupons.js
@@ -1,33 +1,22 @@
 const hre = require("hardhat");
-
 async function main() {
-  const [user] = await hre.ethers.getSigners();
-
-  const contractAddress = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"
-  const Coupon = await hre.ethers.getContractFactory("SimpleCoupon");
-  const coupon = await Coupon.attach(contractAddress);
-
-  // Get only this user's coupons
-  const ownedCoupons = await coupon.getMyCoupons();
-
-  if (ownedCoupons.length === 0) {
-    console.log("No coupons found for this user.");
-    return;
-  }
-
-  for (let i = 0; i < ownedCoupons.length; i++) {
-    const couponData = ownedCoupons[i];
-    console.log(`Coupon #${i}`);
-    console.log(`- Code: ${couponData.code}`);
-    console.log(`- Description: ${couponData.description}`);
-    console.log(`- Image: ${couponData.image}`);
-    console.log(`- Used: ${couponData.isUsed}`);
-    console.log(`- Owner: ${couponData.owner}`);
-    console.log("");
-  }
+    const [user] = await hre.ethers.getSigners();
+    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const Coupon = await hre.ethers.getContractFactory("SimpleCoupon");
+    const coupon = await Coupon.attach(contractAddress);
+    const result = await coupon.getMyCoupons();
+    console.log(`Coupon of ${user.address}`);
+    result.forEach((coupon, index) => {
+        console.log(`--- Coupon #${index} ---`);
+        console.log(`Code: ${coupon.code}`);
+        console.log(`Description: ${coupon.description}`);
+        console.log(`Used: ${coupon.isUsed}`);
+        console.log(`Owner: ${coupon.owner}`);
+        console.log('');
+    });
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
